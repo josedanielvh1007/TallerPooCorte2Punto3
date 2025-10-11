4bd58847c15faece4200c091d0c92a8186e7c11c
Jose Daniel <josedanielvh1007@gmail.com>
2025-10-11 18:56:18 -0500

    README.md

---
+++ src/main/java/com/uao/punto3tallercorte2/Punto3TallerCorte2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.punto3tallercorte2;
+
+/**
+ *
+ * @author jose
+ */
+public class Punto3TallerCorte2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
